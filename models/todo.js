7ac83b0e4c7b492c7fe332abb8a6948fef34712b
ludndev <ludndev@gmail.com>
2023-05-12 12:54:01 +0100

    fix: setup knex for objection model

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,9 @@
 const { Model } = require('objection');
 
+const Knex = require('knex');
+const knexConfig = require('./../knexfile');
+Model.knex(Knex(knexConfig.development));
+
 class Todo extends Model {
     static get tableName() {
         return 'todos';
