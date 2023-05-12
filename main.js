8c8566482746d0f66178a8e46fa975dbdd9ea200
ludndev <ludndev@gmail.com>
2023-05-12 13:09:36 +0100

    feat: add json and cors middlewares

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
+const json = require('koa-json');
 const defaultRoutes = require('./routes/default');
 const todoRoutes = require('./routes/todo');
 
@@ -10,7 +12,9 @@ dotenv.config();
 
 const app = new Koa();
 
-// apply body parser middleware
+// apply middlewares
+app.use(cors());
+app.use(json());
 app.use(bodyParser());
 
 // apply routes
