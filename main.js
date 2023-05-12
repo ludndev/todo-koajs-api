061bb99b3f47d67e922fb8f9eefa4573f1c3befa
ludndev <ludndev@gmail.com>
2023-05-12 12:00:33 +0100

    feat: use of bodyparser as middleware

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const dotenv = require('dotenv');
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 
 
 // load .env to process env
 dotenv.config();
 
 const app = new Koa();
+
+// apply body parser middleware
+app.use(bodyParser());
 
 app.use(async ctx => {
     ctx.body = 'Hello World';
