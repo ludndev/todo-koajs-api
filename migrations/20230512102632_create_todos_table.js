453a24705e64a3611e9222579b5a2db8a3906d37
ludndev <ludndev@gmail.com>
2023-05-12 12:10:16 +0100

    feat: todo migration

---
+++ migrations/20230512102632_create_todos_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('todos', function(table) {
+        table.increments('id').primary();
+        table.string('title').notNullable();
+        table.boolean('completed').defaultTo(false);
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('todos');
+};
